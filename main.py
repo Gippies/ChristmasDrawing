4185db3d614ab5a6a0740bb31b84b46373902fa3
Joshua Patterson <jleegippies@gmail.com>
2021-01-12 20:43:17 -0600

    Added colors to terminal

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from colorama import init as colorama_init, Fore
 from openpyxl import load_workbook
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
@@ -7,9 +8,8 @@ def match_people():
     Tk().withdraw()
     fn = askopenfilename(title="Select an Excel file", filetypes=(("Excel Files", "*.xlsx*"), ("All Files", "*.*")))
 
-    print(fn)
     if fn == '':
-        print("No file selected. Exiting...")
+        print(Fore.RED + "No file selected. Exiting...")
         return
 
     wb = load_workbook(fn, read_only=True)
@@ -17,21 +17,30 @@ def match_people():
     print("Calculating worksheet dimensions...")
     sheet_dimensions = sheet.calculate_dimension().split(':')
     if sheet_dimensions[0] != 'A1' or sheet_dimensions[1] == 'A1':
-        print("Warning: worksheet dimensions appear to be incorrect, resetting dimensions and recalculating...")
+        print(Fore.YELLOW + "Warning: worksheet dimensions appear to be incorrect,"
+                            "resetting dimensions and recalculating...")
         sheet.reset_dimensions()
         try:
             sheet.calculate_dimension(force=True)
         except UnboundLocalError:
-            print("It appears you may be using an empty (or very broken) Excel spreadsheet."
+            print(Fore.RED + "It appears you may be using an empty (or very broken) Excel spreadsheet."
                   "Try adding some data or using one that's not broken. Exiting...")
             return
 
-    print("Dimensions calculated successfully. Beginning pairing process...")
+    print(Fore.GREEN + "Dimensions calculated successfully. Beginning pairing process...")
+    people_dict = {}
     for row in sheet.rows:
-        for cell in row:
-            print(cell.value)
+        people_dict[row[0].value] = []
+        for cell in row[1:]:
+            if cell.value is not None:
+                people_dict[row[0].value].append(cell.value)
+
+    individuals = list(people_dict.keys())
+    print("People to pair: " + str(individuals))
+    print("People with exclusions: " + str(people_dict))
     wb.close()
 
 
 if __name__ == '__main__':
+    colorama_init(autoreset=True)
     match_people()
