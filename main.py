8db423df06002dae46f41144a70df429216bc143
Joshua Patterson <jleegippies@gmail.com>
2021-01-12 18:29:51 -0600

    Switched to openpyxl

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import csv
+from openpyxl import load_workbook
 
 
 def match_people():
-    with open("Testfile.csv", newline="") as csv_file:
-        reader = csv.reader(csv_file)
-        people = {}
-
-        # First, extract the people from the csv and build a dictionary with people they're not to be paired with.
-        for line in reader:
-            if line[0] in people.keys():
-                print("ERROR, ERROR. There are duplicate people listed in the provided file. Exiting...")
-                return
-            people[line[0]] = []
-            for person in line[1:]:
-                people[line[0]].append(person)
-        print(people)
+    wb = load_workbook("TestFile.xlsx")
+    sheet = wb.worksheets[0]
+    print(sheet['A5'].value)
 
 
 if __name__ == '__main__':
