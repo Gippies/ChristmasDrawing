5331ff7371247aa4915505181baa64aecd615c57
Joshua Patterson <jleegippies@gmail.com>
2021-01-12 18:06:59 -0600

    Setup a basic beginning

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import csv
+
+
 def match_people():
-    f = open("Testfile.txt", "r")
-    for line in f:
-        print(line)
-    f.close()
+    with open("Testfile.csv", newline="") as csv_file:
+        reader = csv.reader(csv_file)
+        people = {}
+
+        # First, extract the people from the csv and build a dictionary with people they're not to be paired with.
+        for line in reader:
+            if line[0] in people.keys():
+                print("ERROR, ERROR. There are duplicate people listed in the provided file. Exiting...")
+                return
+            people[line[0]] = []
+            for person in line[1:]:
+                people[line[0]].append(person)
+        print(people)
 
 
 if __name__ == '__main__':
