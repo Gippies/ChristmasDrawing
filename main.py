7035a94a442ff4534c74cee95089280ebd33c8ab
Joshua Patterson <jleegippies@gmail.com>
2021-01-12 19:47:11 -0600

    Setup dimension calculations and reading the sheet.

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from openpyxl import load_workbook
 
 
 def match_people():
-    wb = load_workbook("TestFile.xlsx")
+    wb = load_workbook("TestFile.xlsx", read_only=True)
     sheet = wb.worksheets[0]
-    print(sheet.cell(row=1, column=1).value)
+    print("Calculating worksheet dimensions...")
+    sheet_dimensions = sheet.calculate_dimension().split(':')
+    if sheet_dimensions[0] != 'A1' or sheet_dimensions[1] == 'A1':
+        print("Warning: worksheet dimensions appear to be incorrect, resetting dimensions and recalculating...")
+        sheet.reset_dimensions()
+        sheet.calculate_dimension(force=True)
+    for row in sheet.rows:
+        for cell in row:
+            print(cell.value)
+    wb.close()
 
 
 if __name__ == '__main__':
